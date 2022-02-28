3ff844245836cdd20aedef619acba424f703b7cb
Dominique <dominique@oger.ch>
2022-02-28 16:50:37 +0100

    Complete review of the script, in order to - check that all required packages are installed - provire a more reliable testing of the insertion of the USB key - support the SanDisk and the Corsair USB keys - provide more consistent use of the dialog messages

--- CreateFSKeyZST.sh
+++ CreateFSKeyZST.sh
@@ -2,9 +2,13 @@
 #
 # Création de clé Free-Solutions OS bash
 #
-# Necesssite curl, pv , unzstd
+# Nécesssite curl, pv, dialog, unzstd
 # Gain 1623MB de moins 
 # 
+
+# Valid usb key brands
+KEYS=$(echo -e "Corsair\nSanDisk")
+
 if [ $(id -u) -ne 0 ]; then
         echo "Ce programme nécessite des accès privilégiés."
         echo "Redémarrez ce programme avec sudo ou en temps qu'utilisateur 'root'"
@@ -16,73 +20,122 @@ if [ "X${XDG_RUNTIME_DIR}" = "X" ]; then
         fi
         export XDG_RUNTIME_DIR=/tmp/runtime-root
 fi
+
+clear
+
 #
+# Check, and if necessary, install packages 'pv', 'dialog', 'curl' and 'gzip'
 #
-# Install et check package pv
+if dpkg -l | grep "pv" | grep "pipeline" >/dev/null; then
+	echo "Le package pv est installé :)"
+else
+	echo "INSTALLATION du package pv" && apt install -y pv
+fi
+if dpkg -l | grep "user-friendly dialog"  | grep -v whiptail >/dev/null; then
+	echo "Le package dialog est installé :)"
+else
+	echo "INSTALLATION du package dialog" && apt install -y dialog
+fi
+if dpkg -l | grep "curl"  | grep "command line tool" >/dev/null; then
+	echo "Le package curl est installé :)"
+else
+	echo "INSTALLATION du package curl" && apt install -y curl
+fi
+if dpkg -l | grep "zstd" | grep -v libzstd1 >/dev/null; then
+	echo "Le package zstd est installé :)"
+else
+	echo "INSTALLATION du package zstd" && apt install -y zstd
+fi
+
 #
-if dpkg -l | grep "pv" | grep "pipeline" >/dev/null ; then echo "Le package pv est installé :)" ; else echo "INSTALLATION du package pv" && apt install -y pv ; fi
-if dpkg -l | grep "curl" | grep "pipeline" >/dev/null ; then echo "Le package curl est installé :)" ; else echo "INSTALLATION du package curl" && apt install -y curl; fi
-#apt install dialog
-if dpkg -l | grep "user-friendly dialog"  | grep -v whiptail >/dev/null ; then echo "Le package dialog est installé :)" ; else echo "INSTALLATION du package dialog" && apt install -y dialog ; fi
-if dpkg -l | grep "zstd" | grep -v libzstd1 >/dev/null ; then echo "Le package zstd est installé :)" ; else echo "INSTALLATION du package zstd" && apt install -y zstd ; fi
+# Detect the key to be created ...
 #
-echo "Branchez une Clé USB Sandisk Ultrafit 32GB ou Plus"
-while [ ! -e /dev/sd[a-z] ] ; do echo "VEUILLEZ BRANCHEZ UNE CLEF USB ULTRAFIT 32GB ou +" && sleep 1 ; done
-#time curl -N -s https://www.free-solutions.ch/GREEN_SPIDER/4.0/GREEN_SPIDER_5.0.3.dd.gz | gunzip -c | pv -B32M >
-DRIVEUSB=`lsblk | grep disk | awk '{ print $1 }' | grep sd | tail -1l`
-DEVUSB=/dev/$DRIVEUSB
-MANUFACTURER=`dmesg | grep usb | grep Manufacturer | grep -v Linux | awk '{ print $5" "$6 }' | tail -1l`
-PRODUCT=`dmesg | grep usb | grep Product: | grep -v Linux | awk '{ print $5" "$6 }' | tail -1l `
-SECTORS=`fdisk -l /dev/$DRIVEUSB | head -1l | awk '{ print $6 }' | grep -o -E '[0-9]+'`
-DETECTEDTYPE="INCONNU"
-# Test du type de clé 32GB Sandisk
-#if [ $MANUFACTURER = "SanDisk" ]
-if [ `lsusb | grep 0781 | awk '{ print $6}' | tail -1l` ]
-then echo "SANDISK KEY DETECTED" && echo "SECTEURS "$SECTORS
-if [ $SECTORS = 60062500 ]
-then DETECTEDTYPE="sandisk_ultra32_old"
-fi
-if [ $SECTORS = 60063744 ]
-then DETECTEDTYPE="sandisk_ultra32"
-fi
-if [ $SECTORS = 60088320 ]
-then DETECTEDTYPE="sandisk_ultra32_usb31"
-fi
-if [ $SECTORS = 242614272 ]
-then DETECTEDTYPE="sandisk_ultra128_old"
-fi
-if [ $SECTORS = 240353280 ]
-then DETECTEDTYPE="sandisk_ultra128"
-fi
-if [ $SECTORS = 120127488 ]
-then DETECTEDTYPE="sandisk_ultra64"
-fi
-if [ $SECTORS = 488374272 ]
-then DETECTEDTYPE="sandisk_ultra128"
-fi
-fi
-echo "Device USB: "$DEVUSB
-echo "MANUFACTURER: "$MANUFACTURER
-echo "PRODUCT: " $PRODUCT
-echo "Modéle de clé détecté :" $DETECTEDTYPE
-if [ $DETECTEDTYPE = "sandisk_ultra32_old" ] || [ $DETECTEDTYPE = "sandisk_ultra32" ]
+echo "Recherche de la clé ..."
+NBUSB=$(lsusb | grep -e "${KEYS}" | wc -l)
+while [ ${NBUSB} -ne 1 ]; do
+	if [ ${NBUSB} -gt 1 ]; then
+		echo "Trouvé plusieurs clés USB ! Retirez la(les) clé(s) en trop svp"
+	else
+		echo "Branchez une clé USB Sandisk Ultrafit ou une clé Corsair Padlock svp"
+	fi
+	sleep 1
+	NBUSB=$(lsusb | grep -e "${KEYS}" | wc -l)
+done
+sleep 2		# Key found, leave a bit of time for the key to be reachable !
+
+#
+# Find the drive where the key is inserted
+#
+MANUFACTURER=$(lsblk -o NAME,VENDOR | grep -e "${KEYS}" | awk '{ print $2 }')
+DEVUSB=/dev/$(lsblk -o NAME,VENDOR 2>/dev/null | grep -e "${KEYS}" | awk '{ print $1 }')
+
+#
+# Find details about the key
+#
+PRODUCT=$(lsblk -o MODEL ${DEVUSB} | sed '/^$/d' | grep -v MODEL)
+SECTORS=$(fdisk -l ${DEVUSB} 2>/dev/null | grep ${DEVUSB} | awk '{ print $7 }' | head -1)
+
+DETECTEDTYPE="Inconnu"
+case "${MANUFACTURER}" in
+	"SanDisk")
+		case ${SECTORS} in
+		60062500)
+			DETECTEDTYPE="ultra32_old";;
+		60063744)
+			DETECTEDTYPE="ultra32";;
+		60088320)
+			DETECTEDTYPE="Ultra 32 GB";;
+		120127488)
+			DETECTEDTYPE="Ultra 64 GB";;
+		240353280)
+			DETECTEDTYPE="Ultra 128 GB";;
+		242614272)
+			DETECTEDTYPE="Ultra 128 GB";;
+		488374272)
+			DETECTEDTYPE="Ultra 256 GB";;
+		esac;;
+	"Corsair")
+		case ${SECTORS} in
+		60518400)
+			DETECTEDTYPE="32 GB";;
+		120913920)
+			DETECTEDTYPE="64 GB";;
+		esac;;
+esac
+
+echo "Périphérique USB : "${DEVUSB}
+echo "Fabricant        : "${MANUFACTURER}
+echo "Produit          : "${PRODUCT}
+echo "Modèle           : "${DETECTEDTYPE}
+
+#
+# Discard old key models, they don't have enough capacity ...
 # Il nous faut au minimum 60088320 secteurs de 512, les anciennes clés Sandisk 32GB ne fonctioneront pas
-then echo "Clé Sandisk ancienne : MODELE INCORRECT Merci d'utiliser le dernier modèle Sandisk Ultra 32GB !!!"  && exit
+#
+if [ "${DETECTEDTYPE}" = "Inconnu" ]; then
+	echo "Modèle de clé inconnu : merci d'utiliser un modèle supporté par le programme"
+	exit
 fi
-printf "\nVous pouvez tenter de créer votre clef USB Free-Solutions OS"
+if [ "${DETECTEDTYPE}" = "ultra32_old" ] || [ "${DETECTEDTYPE}" = "ultra32" ]; then
+	echo "Clé Sandisk ancienne : merci d'utiliser un modèle plus récent de Sandisk Ultra 32GB"
+	exit
+fi
+
+printf "\nVous allez démarrer la création de votre clef USB Free-Solutions OS"
 printf "\nVous avez besoin d'une très bonne connection Internet, sans interruptions"
 printf "\nAUCUN FICHIER n'est chargé sur votre machine, le code est directement écrit depuis Internet sur la clé USB\n\n"
 
-#echo "ETES VOUS SUR et CERTAIN de VOULOIR EFFACER :" $DEVUSB ?
-read -p "Continuer et effacer intégralement $DEVUSB (O/N)?"
+read -p "Continuer et effacer intégralement ${DEVUSB} (O/N) ? "
 if [ "$REPLY" != "O" ]; then
-   echo "Sortie - Abandon " exit
-else
-	echo "Creation de votre clé Free-Solutions OS..."
-	echo "Attention le temps d'execution est long voire très long..."
-	echo ""
-        curl -N -s https://www.free-solutions.ch/GREEN_SPIDER/GREEN_SPIDER_RELEASE/GREEN_SPIDER.dd.zst | unzstd -c | (pv -B32M -n -  > $DEVUSB  conv=notrunc,noerror) 2>&1 | dialog --title "Creation de Free-Solutions OS sur USB" --gauge "\nCréation Clé Free-Solutions OS en cours Veuillez patienter...\n\nBien attendre jusqu'au message :\n Votre clé Bootable Free-Solutions OS est prête à être bootée  ENJOY" 10 75 0 
+	echo "Abandon de la procédure de création de clé"
+	exit
 fi
-sync
-dialog --infobox "\nVotre clé Bootable Free-Solutions OS est prête à être bootée !!! ENJOY" 5 70 
+	
+echo "Creation de votre clé Free-Solutions OS..."
+echo "Attention le temps d'execution est long, voire très long..."
 echo ""
+curl -N -s https://www.free-solutions.ch/GREEN_SPIDER/GREEN_SPIDER_RELEASE/GREEN_SPIDER.dd.gz | unzstd -c | (pv -B32M -n - >${DEVUSB} conv=notrunc,noerror) 2>&1 | dialog --title "Creation de Free-Solutions OS sur votre clé USB" --gauge "\nCréation Clé Free-Solutions OS en cours Veuillez patienter...\n\nBien attendre jusqu'au message :\n Votre clé Bootable Free-Solutions OS est prête à être bootée !!! ENJOY" 10 75 0 
+
+sync
+dialog --msgbox "Votre clé Bootable Free-Solutions OS est prête à être bootée !!! ENJOY.\nAppuyer sur 'return' pour terminer le programme" 0 0
+clear
